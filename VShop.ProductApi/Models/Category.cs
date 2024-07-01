47bc8d1d3201b0dbb2734eb75c980127f4483694
Fábio simones <simones.fabio1994@gmail.com>
2024-07-01 16:26:06 -0300

    Implementação de DTOs, Interfaces e Repositórios.

--- VShop.ProductApi/Models/Category.cs
+++ VShop.ProductApi/Models/Category.cs
@@ -1,8 +1,10 @@
-﻿namespace VShop.ProductApi.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VShop.ProductApi.Models;
 
 public class Category
 {
-    public int CategoryId { get; set; }
+    public int CategoryId { get; set; }    
     public string? Name { get; set; }
 
     public ICollection<Product>? Products { get; set; }
